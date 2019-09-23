a58e3d028d2b1049d9ec7c82eff18369a9ff20af
Sachin <sachin.cloudnaut@gmail.com>
2019-09-23 22:44:10 +0530

    newe sdk code 4

--- src/lib/1.js
+++ src/lib/1.js
@@ -1,7 +1,11 @@
+var Two = require('./2.js')
 function One(){
 
 }
 One.prototype.print = function(){
+    var t = new Two()
+    t.print()
     console.log('1. print')
+
 }
 module.exports = One;
---
+++ src/lib/2.js
@@ -0,0 +1,7 @@
+function Two(){
+
+}
+Two.prototype.print = function(){
+    console.log('2. print')
+}
+module.exports = Two;
