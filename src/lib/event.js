eed8b8c099640371f5bcafef48ed25c0e0cf42c1
Sachin <sachin.cloudnaut@gmail.com>
2019-09-24 13:51:31 +0530

    change 11

--- src/lib/event.js
+++ src/lib/event.js
@@ -10,11 +10,16 @@ function Event() {
 Event.prototype.push = function(name, source, event_data){
     console.log('event.push called')
     const queue = new SqsHandler();
-    queue.push(name,source,event_data).then(function(data){
-        console.log("Success ", data.MessageId);
-    }).catch(function(err){
-        console.log(err)
-    });
+    return new Promise(function(fulfill, reject) {
+        queue.push(name,source,event_data)
+        .then(function(data){
+            console.log("Success ", data.MessageId);
+            fulfill(data)
+        }).catch(function(err){
+            console.log(err)
+            reject(err);
+        });
+    })
 }
 
 module.exports = Event;
